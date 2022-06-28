8b1a06c1a0b9faba90f10fe0e2804d2f2faaae24
rijokpaul <rijokpaul@gmail.com>
2022-06-28 18:43:43 +0530

    variants order correction

--- src/utils/VariantUtil.js
+++ src/utils/VariantUtil.js
@@ -4,7 +4,16 @@ class VariantUtil {
     }
 
     getVariations() {
-      return (this.product.options !== undefined ? this.product.options : []);
+      const order = { "XS": 1, "S": 2, "M": 3, "L": 4, "XL": 5};
+      const result = (this.product.options !== undefined ? this.product.options : []);
+      const sizeOption = result.find(item => item.name === 'Size').values;
+      if(sizeOption) {
+        sizeOption.sort((a, b) => {
+          return (order[a] || Number.MAX_VALUE) - (order[b] || Number.MAX_VALUE)
+        })
+      }
+      return result;
+      // return (this.product.options !== undefined ? this.product.options : []);
     }
 
     selectVariant(selectedCombination) {
