cc9f3e8c8bafe7a61a31a93f897e06e394d45eb1
rijokpaul <rijokpaul@gmail.com>
2022-06-30 19:12:47 +0530

    localstorage cart

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import Cart from './utils/Cart'
 
 Vue.config.productionTip = false
 
+const savedCartItems = localStorage.getItem('cartItems');
+if(savedCartItems === undefined || savedCartItems === null) {
+  localStorage.setItem('cartItems', JSON.stringify([]));
+}
+
 const cart = new Cart();
 Vue.prototype.$cart = cart
 Vue.prototype.$miniCart = Vue.observable({
